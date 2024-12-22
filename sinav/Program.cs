c00fdfe90375a3590a01f0c22929db56dee9ba07
selam <hoigjgrxkj@gmail.com>
2024-12-22 06:42:53 -0800

    Add loading frame

--- sinav/Program.cs
+++ sinav/Program.cs
@@ -16,7 +16,7 @@ namespace sinav
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Student_Login());
+            Application.Run(new loading_page());
         }
     }
 }
